17dfc36307c00d78c850a44c5bff920d2a89165c
KiranDatadetective <kiranbabukk7@gmail.com>
2022-01-11 21:03:45 +0530

    [KB] First commit

---
+++ src/main/java/com/example/hystrixapp/HystrixappApplication.java
@@ -0,0 +1,13 @@
+package com.example.hystrixapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HystrixappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hystrixapp/HystrixappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hystrixapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
